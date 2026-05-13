817995c42c1b80b698884edc2b8bc6287b096f71
PEGASUS <youssefb354@gmail.com>
2026-05-13 11:44:12 +0100

    Add Pinterest tracking tag

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -67,6 +67,31 @@ export default async function LocaleLayout({
           strategy="lazyOnload"
           crossOrigin="anonymous"
         />
+
+        {/* Pinterest Tag using Next.js Script */}
+        <Script id="pinterest-tag" strategy="afterInteractive">
+          {`
+            !function(e){if(!window.pintrk){window.pintrk = function () {
+            window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
+            n=window.pintrk;n.queue=[],n.version="3.0";var
+            t=document.createElement("script");t.async=!0,t.src=e;var
+            r=document.getElementsByTagName("script")[0];
+            r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
+            pintrk('load', '2613026448211');
+            pintrk('page');
+          `}
+        </Script>
+        
+        {/* The NoScript Image Fallback for Pinterest */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://ct.pinterest.com/v3/?event=init&tid=2613026448211&noscript=1"
+          />
+        </noscript>
       </head>
       <body className="flex flex-col min-h-full bg-base text-graphite font-figtree antialiased">
         <NextIntlClientProvider locale={locale} messages={messages}>
